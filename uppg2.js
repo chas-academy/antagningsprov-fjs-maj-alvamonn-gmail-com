51f47f72b75ac5b4d820654f052293020cf0019c
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-30 12:59:58 +0200

    uppdateringar uppg2

--- uppg2.js
+++ uppg2.js
@@ -3,9 +3,9 @@ function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
    const name = "Älva"
-   return name
+   console.log(name)
 }
 
-console.log(uppg2())
+uppg2()
 
 module.exports = { uppg2 };
