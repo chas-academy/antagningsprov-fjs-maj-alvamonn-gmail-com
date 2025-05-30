d1f8d392c4b74ae3e212db04aa57b52c05f99758
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-30 09:44:49 +0200

    klar

--- uppg6.js
+++ uppg6.js
@@ -4,8 +4,13 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+    const names = ["Mickey", "Duffy", "PowerPuff", "ReesesPieces", "Scooby"]
+    
+    for (const name of names){
+        console.log(name)
+    }
 }
+
+uppg6()
 
 module.exports = { uppg6 };
