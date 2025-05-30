dc799a9d73bf7b847205ec12330754d7351e9f4c
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-30 09:20:07 +0200

    klar

--- uppg2.js
+++ uppg2.js
@@ -2,8 +2,10 @@
 function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
-
-
+   const name = "Älva"
+   return name
 }
+
+console.log(uppg2())
 
 module.exports = { uppg2 };
