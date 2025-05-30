cf6a0696885f2ed8fc994b5f264bca5217f2cd1c
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-30 09:33:06 +0200

    klar

--- uppg5.js
+++ uppg5.js
@@ -4,8 +4,21 @@ function uppg5() {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
+    //Exempeltal
+    const nr1 = 10
+    const nr2 = 8
 
+    if (nr1 > nr2){
+        console.log("Det första talet är större än det andra")
+    }
+    else if(nr2 > nr1){
+        console.log("Det andra talet är större än det första")
+    }
 
+    //Här skulle jag, utöver uppgiftens instruktioner, placera en "else" för om talen är lika stora
+    //och skriva ut "Talen är lika stora"
 }
+
+uppg5()
 
 module.exports = { uppg5 };
