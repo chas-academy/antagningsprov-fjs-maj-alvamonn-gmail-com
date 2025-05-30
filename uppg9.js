98e6958b7dfc365123625c8aaef59b5824417312
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-30 12:06:00 +0200

    klar

--- uppg9.js
+++ uppg9.js
@@ -8,7 +8,24 @@ function uppg9 (){
 
     // anropa funktionen och skicka med en array som argument
 
+    function sort(array){
+
+        for(let i of array){
+            if (i % 2 !== 1){ //om resten av talet dividerat med 2 inte är 1
+                console.log(i + " jämnt")
+            }
+            else{ //resten skulle isf bli 1
+                console.log(i + " udda")
+            }
+        }
+    }
+
+    let numbers = [20, 41, 2, 32, 71, 93,] //lägger till en array med nummer
+
+    sort(numbers)
   
 }
+
+uppg9()
 
 module.exports = { uppg9 };
